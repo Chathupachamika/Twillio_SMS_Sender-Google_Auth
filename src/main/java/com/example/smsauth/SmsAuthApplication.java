423aeb49468ef6d158c312e9f472b1e5255aac78
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-17 00:09:56 +0530

    update

--- src/main/java/com/example/smsauth/SmsAuthApplication.java
+++ src/main/java/com/example/smsauth/SmsAuthApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmsAuthApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SmsAuthApplication.class, args);
 	}
+
 }
